7636886a218d275979f79a09cbde415e4b409830
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2022-07-18 12:41:03 -0400

    Imported and created Error Handling

--- bugtracker-api/server.js
+++ bugtracker-api/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const PORT = 3000
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`🚀 Server listening at http://localhost:${PORT}`)

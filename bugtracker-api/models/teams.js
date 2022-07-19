fb5cf14627390b777f140e4efa77e26b85d1a4e3
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2022-07-19 00:48:11 -0400

    Updating teams endpoint (in progress)

--- bugtracker-api/models/teams.js
+++ bugtracker-api/models/teams.js
@@ -8,9 +8,30 @@ class Teams
         //List all teams a user belongs to
     }
 
-    static async createTeam()
+    static async createTeam({user, teamInfo})
     {
-        //Create a new team
+        const requiredFields = ["name", "members", "projects"]
+        requiredFields.foreach((field) => {
+            if(!teamInfo.hasOwnProperty(field))
+            {
+                throw new BadRequestError(`Required field ${field} missing from request!`)
+            }
+        })
+
+        const results = await db.query(
+            `
+                INSERT INTO teams
+                (
+                    name,
+                    members,
+                    projects,
+                    creator_id
+                )
+                VALUES($1, $2, $3, (SELECT id FROM WHERE email = $4))
+                RETURNING id, name, members, creator_id, projects
+            `, [teamInfo.name, teamInfo.members, teamInfo.projects, user.email])
+        
+        return results.rows[0]
     }
 
     static async fetchTeamById()
@@ -21,6 +42,23 @@ class Teams
     static async addNewTeamMember()
     {
         //Add a new member to a team
+    }
+
+    static async fetchMembersById(members)
+    {
+        if(!members)
+        {
+            throw new BadRequestError("No list of members' emails was provided!")
+        }
+
+        let membersList = [];
+        const query = `SELECT id FROM WHERE email = $1`
+        members?.map(async (email) => {
+            const result = await db.query(query, [email])
+            membersList.push(result.rows[0])
+        })
+
+        return membersList
     }
 }
 
--- bugtracker-api/routes/team.js
+++ bugtracker-api/routes/team.js
@@ -14,6 +14,9 @@ router.get("/", security.requireAuthenticatedUser, async(req,res,next) => {
         //Grab the user from the local server
         //Get a list of all the teams a user belongs to by calling Teams Class
         //Return a the list back to the Client
+        const {user} = res.locals.user
+        const teamList = await Teams.listAllTeams({user})
+        return res.status(200).json({teamList: teamList})
     }
     catch(error)
     {
@@ -33,6 +36,9 @@ router.post("/", security.requireAuthenticatedUser, async(req,res,next) => {
         //Grab the user from the local server
         //Send both the user and team information provided from the Client req body and store it
         //Return a json body with the new team information
+        const {user} = res.locals.user
+        const team = await Teams.createTeam({user: user, teamInfo: req.body})
+        return res.status(201).json({team: team})
     }
     catch(error)
     {
@@ -51,6 +57,10 @@ router.get("/:teamId", security.requireAuthenticatedUser, async(req,res,next) =>
         //Extract the teamId from the url parameters
         //fetch the team information using only the teamId and store it
         //Return the team information back to the Client
+
+        const {teamId} = req.params
+        const team = Teams.fetchTeamById(teamId)
+        return res.status(200).json({team: team})
     }
     catch(error)
     {

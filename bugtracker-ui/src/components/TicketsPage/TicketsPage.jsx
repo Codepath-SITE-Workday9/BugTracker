74e8a4021a297994168ba6493f53edb19fbc5a12
AaronAlemi <alemiaaron@gmail.com>
2022-07-20 16:50:03 -0400

    Worked on dashboard, tickets page, and importing table library

--- bugtracker-ui/src/components/TicketsPage/TicketsPage.jsx
+++ bugtracker-ui/src/components/TicketsPage/TicketsPage.jsx
@@ -1,3 +1,26 @@
+import React from "react";
+import "./TicketsPage.css"
+
 export default function TicketsPage() {
-  return <div className="tickets-page">Tickets Page</div>;
+  return (
+    <div className="tickets-page">
+      <div className="row">
+
+        <div className="projects-box">
+          <div className="project-box-header">
+            <h1>Selected Project: Project 1</h1>
+            <button>Select Project</button>
+          </div>
+          <div className="project-box-content">
+            
+          </div>
+        </div>
+
+        <div className="tickets-box">Tickets Box</div>
+      </div>
+      <div className="row">
+        <div className="ticket-info-box">Ticket Info Box</div>
+      </div>
+    </div>
+  )
 }
---
+++ bugtracker-ui/src/services/MTable.js
@@ -0,0 +1,8 @@
+import MaterialTable from 'material-table';
+import { data } from '../data';
+
+const MTable = () => {
+    //...
+};
+
+export default MTable;

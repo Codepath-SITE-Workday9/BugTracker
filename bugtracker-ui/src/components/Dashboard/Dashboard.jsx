efd8deb8679ee185128f643eb71b749d0067f427
AaronAlemi <alemiaaron@gmail.com>
2022-07-21 14:23:18 -0400

    Successfully implemented and added  material-table library tables into dashboard

--- bugtracker-ui/src/components/Dashboard/Dashboard.jsx
+++ bugtracker-ui/src/components/Dashboard/Dashboard.jsx
@@ -3,9 +3,11 @@ import "./Dashboard.css";
 import { useOpenContext } from "../../contexts/open";
 import { useEffect } from "react";
 import "../../services/charts.js";
-// import { BasicTable } from "../Tables/BasicTable";
+ import { BasicTable } from "../Tables/BasicTable";
+ import { DashboardProjectsTable } from "../Tables/dashboardProjectsTable";
 //import MaterialTable from 'material-table';
 import { data } from "../../sampleData";
+import { DashboardTeamsTable } from "../Tables/DashboardTeamsTable";
 
 export default function Dashboard() {
   const { isOpen } = useOpenContext();
@@ -21,7 +23,42 @@ export default function Dashboard() {
 
   return (
     <div className={isOpen ? "dashboard open" : "dashboard closed"}>
-      <div className="projects-table">
+      <DashboardProjectsTable />
+
+
+      <div className="ticket-statistics">
+        <h>TICKET STATISTICS</h>
+        <div className="statistics-row">
+          <canvas
+            className="donut-chart"
+            id="category-chart"
+            width="800"
+            height="450"
+          ></canvas>
+          <br />
+          <canvas
+            className="donut-chart"
+            id="status-chart"
+            width="800"
+            height="450"
+          ></canvas>
+          <br />
+          <canvas
+            className="donut-chart"
+            id="priority-chart"
+            width="800"
+            height="450"
+          ></canvas>
+        </div>
+      </div>
+
+      <DashboardTeamsTable />
+    </div>
+  );
+}
+
+/*
+  <div className="projects-table">
         <div className="header-row">
           <h>YOUR PROJECTS</h>
           <div className="project-search">
@@ -88,7 +125,10 @@ export default function Dashboard() {
         </div>
       </div>
 
-      <div className="ticket-statistics">
+*/
+
+/*
+<div className="ticket-statistics">
         <h>TICKET STATISTICS</h>
         <div className="statistics-row">
           <canvas
@@ -113,8 +153,10 @@ export default function Dashboard() {
           ></canvas>
         </div>
       </div>
+*/
 
-      <div className="teams-table">
+/*
+<div className="teams-table">
         <div className="header-row">
           <h>YOUR TEAMS</h>
           <div className="project-search">
@@ -183,7 +225,4 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
-      <BasicTable />
-    </div>
-  );
-}
+*/
--- bugtracker-ui/src/components/Tables/BasicTable.jsx
+++ bugtracker-ui/src/components/Tables/BasicTable.jsx
@@ -1,21 +1,34 @@
 import MaterialTable from "material-table";
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
-const handleOnRowClick = (e) => {
+
+const handleOnRowClick = (rowData) => {
   console.log("Clicked on row!")
+  console.log(rowData.id)
+}
+
+function onRowClick(data) {
+  console.log("Row data below!")
+  console.log(data)
 }
 
 const data = [
-  { name: "Mohammad", surname: "Faisal", birthYear: 1995 },
-  { name: "Nayeem Raihan ", surname: "Shuvo", birthYear: 1994 },
+  { id: 1, name: "Mohammad", surname: "Faisal", birthYear: 1995 },
+  { id: 2, name: "Nayeem Raihan ", surname: "Shuvo", birthYear: 1994 },
 ];
 
 const columns = [
-  { title: "Name", field: "name" },
+  { title: "Id", field: "id", hidden: true},
+  { title: "Name", field: "name"  /*, render: row => <div onClick={() => console.log(row.id)}>{row.name}</div> */ },
   { title: "Surname", field: "surname" },
   { title: "Birth Year", field: "birthYear", type: "numeric" },
 ];
 
 export const BasicTable = () => {
-  return <MaterialTable title="Basic Table" columns={columns} data={data} onRowClick={handleOnRowClick} />;
+  return <MaterialTable 
+    title="Basic Table" 
+    columns={columns} 
+    data={data} 
+    onRowClick={(handleOnRowClick, rowData) => onRowClick(rowData)} />
+    
 };

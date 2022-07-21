5c773d6f02d70367c483c0ceff603800dc6625e1
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2022-07-21 18:41:59 -0400

    Testing createTicket branch

---
+++ bugtracker-api/models/ticket.js
@@ -0,0 +1,21 @@
+const db = require("../db")
+const {BadRequestError, NotFoundError} = require("../utils/errors")
+
+class Tickets 
+{
+    static async listAllTickets()
+    {
+        //Function to fetch tickets for a project
+    }
+
+    static async createTicket()
+    {
+        //function to create a new ticket
+        //adding comment to test branch
+    }
+
+
+}
+
+
+module.exports = Tickets

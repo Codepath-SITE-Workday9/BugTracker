eec993a6bc227c705ad476a8e9fcd5b2427e3539
katherinj <katjimenez98@gmail.com>
2022-07-21 13:20:21 -0400

    Added additional styling to TeamView, changed developers on team &project assigned to team view to table view

--- bugtracker-ui/src/components/TeamsPage/TeamView/TeamView.jsx
+++ bugtracker-ui/src/components/TeamsPage/TeamView/TeamView.jsx
@@ -1,11 +1,15 @@
 import "./TeamView.css";
-import TeamModal from "../TeamModal/TeamModal";
+import { useProjectContext } from "../../../contexts/project";
+
 export default function TeamView({ setModal, currentTeam }) {
+  const { projects } = useProjectContext();
+  console.log(projects);
   var devs = [
     { name: "doris", numTickets: 3 },
     { name: "aaron", numTickets: 3 },
     { name: "katherin", numTickets: 2 },
   ];
+
   return (
     <div className="team-view">
       <div className="team-header">
@@ -15,51 +19,123 @@ export default function TeamView({ setModal, currentTeam }) {
         </button>
       </div>
 
-      <div className="team-developers">
-        <h2>Developers on this team: </h2>
-        <div className="developers-container">
-          {devs.map((dev) => (
-            <DeveloperRow name={dev.name} numTickets={dev.numTickets} />
-          ))}
-        </div>
-        <div className="add-developer-to-team">
-          <p> Add a developer to this team: </p>
-          <div className="team-search">
-            <input
-              className="search-input"
-              type="text"
-              name="search"
-              placeholder="developers email"
-              // onChange={handleOnChange}
-            />
-            <i className="material-icons">search</i>
-          </div>
-        </div>
-      </div>
+      <DevelopersOnTeam devs={devs} />
 
-      <div className="team-projects">
-        <h2>Projects assigned to this team: </h2>
+      <AddDeveloperToRow />
+
+      <ProjectsAssignedToTeams projects={projects} />
+    </div>
+  );
+}
+
+export function DevelopersOnTeam({ devs }) {
+  return (
+    <div className="team-developers">
+      <h2>Developers on this team: </h2>
+
+      <div className="developers-table">
+        <table role="table" className="table">
+          <thead>
+            <tr role="row">
+              <th colSpan="1" role="columnheader">
+                Developer
+              </th>
+              <th colSpan="1" role="columnheader">
+                Role
+              </th>
+              <th colSpan="1" role="columnheader">
+                Number of open tickets
+              </th>
+            </tr>
+          </thead>
+          <tbody role="rowgroup">
+            {devs?.map((dev) => (
+              <DeveloperRow
+                name={dev.name}
+                numTickets={dev.numTickets}
+                role="developer"
+              />
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 }
 
-export function DeveloperRow({ name, numTickets }) {
+export function DeveloperRow({ name, numTickets, role }) {
   return (
-    <div className="developer-row">
-      <p> {name}</p>
-      <p>
-        {numTickets} ticket{numTickets == 1 ? "" : "s"} assigned
-      </p>
+    <tr role="row" className="row">
+      <td role="cell">
+        <span class="material-symbols-outlined">face</span>
+        {name}
+      </td>
+      <td role="cell">{role}</td>
+      <td role="cell">{numTickets}</td>
+    </tr>
+  );
+}
+
+export function AddDeveloperToRow() {
+  return (
+    <div className="add-developer-to-team">
+      <p> Add a developer to this team: </p>
+      <div className="dev-search">
+        <input
+          className="search-input"
+          type="text"
+          name="search"
+          placeholder="developers email"
+          // onChange={handleOnChange}
+        />
+        <button className="search-btn">
+          <i className="material-icons">search</i>
+        </button>
+      </div>
     </div>
   );
 }
 
-export function ProjectRow() {
+export function ProjectsAssignedToTeams({ projects }) {
   return (
-    <div className="project-row">
-      <p> Project </p>
-      <p> assigned</p>
+    <div className="team-projects">
+      <h2>Projects assigned to this team: </h2>
+      <div className="team-projects-table">
+        <table role="table" className="table">
+          <thead>
+            <tr role="row">
+              <th colSpan="1" role="columnheader">
+                Project Name
+              </th>
+              <th colSpan="1" role="columnheader">
+                Description
+              </th>
+              <th colSpan="1" role="columnheader">
+                Number of open tickets
+              </th>
+            </tr>
+          </thead>
+          <tbody role="rowgroup">
+            {projects?.map((p) => (
+              <ProjectRow
+                name={p.projectTitle}
+                description={p.description}
+                openTickets={p.tickets}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
+  );
+}
+
+export function ProjectRow({ name, description, openTickets }) {
+  return (
+    <tr role="row" className="row">
+      <td role="cell">{name}</td>
+      <td role="cell">{description}</td>
+      <td role="cell">{openTickets}</td>
+    </tr>
   );
 }

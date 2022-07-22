5b3f108aaa4bed1c3f151db342f601d1b167b884
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2022-07-22 00:53:11 -0400

    Created function in models to add new ticket to a project once created

--- bugtracker-api/models/ticket.js
+++ bugtracker-api/models/ticket.js
@@ -30,22 +30,38 @@ class Tickets
             `
                 INSERT INTO tickets
                 (
-                    developers, 
-                    project_id, 
-                    title, 
-                    description, 
-                    category, 
-                    priority, 
-                    status, 
-                    complexity,
-                    creator_id,
+                    developers, project_id, title, description, category, priority, status, complexity, creator_id,
                     created_by
                 )
                 VALUES( (SELECT ARRAY(SELECT id FROM users WHERE email = any($1))), $2, $3, $4, $5, $6, $7, $8, $9, $9)
                 RETURNING  *
             `, [ticketInfo.developers, ticketInfo.projectId, ticketInfo.title, ticketInfo.description, ticketInfo.category, ticketInfo.priority, ticketInfo.status, ticketInfo.complexity, userId])
-                
-        return results.rows[0]
+        
+        const ticket = results.rows[0]
+        console.log(ticket)
+        Tickets.addTicketToProject(ticket.id, ticketInfo.projectId)
+
+        return ticket
+    }
+
+
+    static async addTicketToProject(ticketId, projectId)
+    {
+        if(!ticketId)
+        {
+            throw new BadRequestError(`Missing the ticket id!`)
+        }
+        else if(!projectId)
+        {
+            throw new BadRequestError(`Missing the project id`)
+        }
+
+        const results = await db.query(
+            `
+                UPDATE projects
+                SET tickets = ARRAY_APPEND(tickets, $1)
+                WHERE id = $2 
+            `, [ticketId, projectId])
     }
 
 

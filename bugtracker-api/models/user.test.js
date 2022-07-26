4b5bb0b1a50459d7583bc8a4cb170d9f8c998b02
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2022-07-26 18:34:21 -0400

    Updated testing suite with new comments to explain what the tests are checking for

--- bugtracker-api/models/user.test.js
+++ bugtracker-api/models/user.test.js
@@ -1,24 +1,46 @@
+//IMPORTED DATABASE, ERROR HANDLING, USER MODELS, AND DATABASE COMMANDS FROM COMMON.JS
 const db = require("../db")
 const {UnauthorizedError, BadRequestError} = require("../utils/errors")
 const User = require("./user")
 const {commonBeforeAll, commonBeforeEach, commonAfterAll, commonAfterEach} = require("../tests/common")
 
 
+
+
+
+//DATABASE COMMANDS - Assures that everytime an action occurs with the database which in this case is a user insertion,
+//Rollback all actions perfomed with the datbase once testing ends
+//Ensures that test can be used repeatedly without the ned to keep error checking working models
 beforeAll(commonBeforeAll)
 afterAll(commonAfterAll)
 beforeEach(commonBeforeEach)
 afterEach(commonAfterEach)
 
+
+
+
+
+//Creates a new user with email and fullName parameters
 const newUser = 
 {
     email: "test_user@gmail.com",
     fullName: "Test User"
 }
 
-describe("User", () => {
+
+
+
+//TEST THE USER MODELS INCLUDING REGISTER AND LOGIN FUNCTIONALITY
+describe("User Models", () => {
+    //Test that a user can be properly registered and will return the correct information back to the client-side
     describe("Test User Registration", () => {
+
+
+        //Test that a user can be registered; Should return the id, email, and fullname of the user
         test("User can register with proper credentials", async () => {
+            //Register the fake user into the database
             const user = await User.register({ ...newUser, password: "pw"})
+            //Register request should return a JSON body containing the new id, email, and fullName of the user
             expect(user).toEqual({
                 id: expect.any(Number),
                 email: newUser.email,
@@ -26,8 +48,14 @@ describe("User", () => {
             })
         })
 
+
+        //Test that a user receives a BadRequestError if they try to register with a pre-existing email
         test("Registering with duplicate email throws an error", async () => {
+            //Make sure that there is only one assertion being received when the function executes
             expect.assertions(1)
+
+            //Register a user into the system and then re-register another user with the same email
+            //Request should throw a BadRequestError detailing that the user provided a duplicate email
             try
             {
                 await User.register({ ...newUser, password: "pw"})

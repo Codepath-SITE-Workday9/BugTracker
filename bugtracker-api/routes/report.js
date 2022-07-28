f98e160c73088bc491ebbcaaffaa967063f29ba5
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2022-07-28 12:38:05 -0400

    Implemented the routes for obtaining ticket statistics for a single project and all projects

--- bugtracker-api/routes/report.js
+++ bugtracker-api/routes/report.js
@@ -19,12 +19,54 @@ router.get("/velocity", security.requireAuthenticatedUser, async(req,res,next) =
     }
 })
 
-//FUNCTION TO OBTAIN THE VELOCITY OF A TEAM IN TERMS OF COMPLETING TICKETS BASED ON COMPLEXITY
+
+
+
+
+
+//FUNCTION TO OBTAIN THE TICKET STATISTICS OF ALL THE PROJECTS A USER IS A CREATOR OR MEMBER OF 
 router.get("/statistics", security.requireAuthenticatedUser, async(req,res,next) => {
     try
     {
+        //Retrieve the user information from the local server
         const {user} = res.locals
+
+        //Call the fetchTicketStatistics function and send in the user information to get ticket statistics for all projects
+        //IF successful, will return an object containing all the tickets filtered by category, priority, and status
+        //If the user is not part of any project, will return an empty array for each category
         const statistics = await Statistics.fetchTicketStatistics({user: user})
+
+        //If successful, returns an array object containing ticket statistics for category, priority, and status for all projects
+        return res.status(200).json({statistics: statistics})
+    }
+    catch(error)
+    {
+        next(error)
+    }
+})
+
+
+
+
+
+
+
+//FUNCTION TO OBTAIN THE TICKET STATISTICS OF A SINGLE PROJECT A USER IS A CREATOR OR MEMBER OF
+router.get("/statistics/:projectId", security.requireAuthenticatedUser, async(req,res,next) => {
+    try
+    {
+        //Retrieve the user information from the local server
+        const {user} = res.locals
+
+        //Retrieve the project id from the url request
+        const {projectId} = req.params
+
+        //Call the fetchStatisticsByProject function to obtain the ticket statistics for a single project if the user has valid access
+        //IF successful, will return an object containing all the tickets filtered by category, priority, and status
+        //If the user is not a member/creator of the request project, throws a not found error detailing invalid access
+        const statistics = await Statistics.fetchStatisticsByProject({user: user, projectId: projectId})
+
+        //If successful, returns an array object containing ticket statistics for category, priority, and status for all projects
         return res.status(200).json({statistics: statistics})
     }
     catch(error)

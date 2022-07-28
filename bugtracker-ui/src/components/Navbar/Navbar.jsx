b2f96890d8b976c3a662943b4402f6cebb936620
katherinj <katjimenez98@gmail.com>
2022-07-28 14:42:26 -0400

    Navbar header changes depending on which page the user is currently on.

--- bugtracker-ui/src/components/Navbar/Navbar.jsx
+++ bugtracker-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,32 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Navbar.css";
 import { useOpenContext } from "../../contexts/open";
 
 export default function Navbar() {
   const { isOpen } = useOpenContext();
+  const [header, setHeader] = useState("DASHBOARD");
+  useEffect(() => {
+    const url = window.location.href;
+    console.log(url);
+    if (url.indexOf(header) == -1) {
+      if (url.indexOf("tickets") > -1) {
+        setHeader("TICKETS");
+      } else if (url.indexOf("projects") > -1) {
+        setHeader("PROJECTS");
+      } else if (url.indexOf("teams") > -1) {
+        setHeader("TEAMS");
+      } else if (url.indexOf("statistics") > -1) {
+        setHeader("STATISTICS");
+      } else if (url.indexOf("dashboard") > -1) {
+        setHeader("DASHBOARD");
+      } else if (url.indexOf("userprofile") > -1) {
+        setHeader("PROFILE");
+      } else if (url.indexOf("settings") > -1) {
+        setHeader("SETTINGS");
+      }
+    }
+  }, [window.location.href]);
 
   return (
     <div className="navbar">
@@ -36,7 +58,7 @@ export default function Navbar() {
             </ul>
             */}
         <div className={isOpen ? "navbar-left-open" : "navbar-left-closed"}>
-          <h>DASHBOARD</h>
+          <h>{header}</h>
         </div>
 
         <div className="navbar-right">

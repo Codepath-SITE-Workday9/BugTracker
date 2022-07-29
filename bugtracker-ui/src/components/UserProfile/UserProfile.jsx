a70e04adba84272d9a0be311bc83bc1c973c886f
DorisSanchez-Velasquez <dorissanchez.velasquez@gmail.com>
2022-07-29 01:06:50 -0400

    Added comments to user profile page and css file

--- bugtracker-ui/src/components/UserProfile/UserProfile.jsx
+++ bugtracker-ui/src/components/UserProfile/UserProfile.jsx
@@ -3,17 +3,19 @@ import { useAuthContext } from "../../contexts/auth";
 import "../UserProfile/UserProfile.css"
 
 export default function UserProfile() {
+  //Pulls the user from the auth context information of the logged in user
   const { user } = useAuthContext();
-  console.log(user)
-  // {user?.imageUrl === null ? (<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwhgREKJpawVaEkD5aRXudpG-Q3gec7qWcSA&usqp=CAU" className="user-image" ></img>) 
-  //                     : (<img src={user.imageUrl} className="user-image" ></img>)}
+
   return (
     <div className="profile-page">
+          {/* Creates a photo banner with background image and user profile photo
+              Conditionally render the image; If no image provided, add a default image for the user */}
           <div class="photo-banner">
-            {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPFZsvVgVpGF7G4AxxzoeG13aPTKBhYm7PAg&usqp=CAU" class="profile-image"></img> */}
             {user?.imageUrl === null ? (<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwhgREKJpawVaEkD5aRXudpG-Q3gec7qWcSA&usqp=CAU" className="profile-image" ></img>) 
                        : (<img src={user.imageUrl} className="profile-image" ></img>)}
           </div>
+
+          {/* All the User Information including their name and email and edit profile button to change user profile information*/}
           <div className="profile-info">
               <h1 class="profile-name">{user.fullName}</h1>
               <p class="email">Email: {user.email}</p>

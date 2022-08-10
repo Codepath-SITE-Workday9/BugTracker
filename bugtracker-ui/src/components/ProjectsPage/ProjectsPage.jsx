a73d6e512915b310968faea48fd222d2b8ad0c76
katherinj <katjimenez98@gmail.com>
2022-08-10 01:07:22 -0400

    Fixed bug in ProjectsOverview where the project list was not being updated when a new project was created

--- bugtracker-ui/src/components/ProjectsPage/ProjectsPage.jsx
+++ bugtracker-ui/src/components/ProjectsPage/ProjectsPage.jsx
@@ -5,127 +5,88 @@ import ProjectsOverview from "./ProjectsOverview/ProjectsOverview";
 import ProjectView from "./ProjectView/ProjectView";
 import ProjectModal from "../Modals/ProjectModal/ProjectModal";
 import { useTicketContext } from "../../contexts/ticket";
-import TicketModal from "../Modals/TicketModal/TicketModal"
+import TicketModal from "../Modals/TicketModal/TicketModal";
 import apiClient from "../../services/apiClient";
 
-// page where a user can see all projects they are apart of(ProjectsOverview component) and where a user can view specific details about a project + create a new project(ProjectView component)
+// page where a user can: see all projects they are apart of(ProjectsOverview component), view specific details about a project (ProjectView component), and create a new project(ProjectModal component)
 export default function ProjectsPage() {
   const {
     projects,
-    setProjects,
     currentProject,
     setCurrentProject,
     projectModal,
     setProjectModal,
     fetchProjects,
     isLoading,
+    sortedProjects,
+    setSortedProjects,
+    sortProjects,
   } = useProjectContext();
 
-  const {ticketModal, selectedProject, setSelectedProject} = useTicketContext()
+  const { ticketModal, selectedProject, setSelectedProject } =
+    useTicketContext();
 
-  const [sortedProjects, setSortedProjects] = useState(projects)
-  // const [selectedProject, setSelectedProject] = useState(-1)
   const [availableMembers, setAvailableMembers] = useState([]);
-
-  // useEffect hook to fetch updated list of projects a user is apart of after creating a new project
-  useEffect(() => {
-    fetchProjects();
-  }, [projectModal, sortedProjects, ticketModal]);
-
-  useEffect(() => {
-    fetchMemsForProject()
-  }, [selectedProject])
 
   // handler function to change which projects's details should be displayed
   const handleOnProjectClick = (projectId) => {
     setCurrentProject(projectId);
-    setSelectedProject(projectId.id)
+    setSelectedProject(projectId.id);
   };
 
-  
+  // handler function to set the selected project when a user selects a project from the dropdown
+  const handleOnProjectChange = (event) => {
+    setSelectedProject(event.target.value.id);
+  };
 
-  //Handler function to change the order of the projects shown in project overview based on the sorting value
-  const handleOnFilterChange = (sortValue) => {
-    if(sortValue === "Least Tickets")
-    {
-      const results = projects.sort((project1, project2) => (project1.tickets.length > project2.tickets.length) ? 1 : -1)
-      setSortedProjects(results)
-    }
-    else if(sortValue === "Most Tickets")
-    {
-      const results = projects.sort((project1, project2) => (project1.tickets.length < project2.tickets.length) ? 1 : -1)
-      setSortedProjects(results)
-    }
-    else if(sortValue === "Most Teams")
-    {
-      const results = projects.sort((project1, project2) => (project1.teams.length > project2.teams.length) ? 1 : -1)
-      setSortedProjects(results)
-    }
-    else if(sortValue === "Least Teams")
-    {
-      const results = projects.sort((project1, project2) => (project1.teams.length < project2.teams.length) ? 1 : -1)
-      setSortedProjects(results)
-    }
-  }
+  const appendToEmailArray = async (teamId) => {
+    const { data, error } = await apiClient.fetchMemberList(teamId);
+    data.teamsData.map((member) => {
+      if (!availableMembers.includes(member.email)) {
+        setAvailableMembers((prev) => [...prev, member.email]);
+      }
+    });
+  };
 
-    // handler function to set the selected project when a user selects a project from the dropdown
-    const handleOnProjectChange = (event) => {
-      setSelectedProject(event.target.value.id);
-    };
+  // useEffect hook to fetch updated list of projects a user is apart of after creating a new project
+  useEffect(() => {
+    sortProjects(projects);
+  }, [projectModal, ticketModal]); // possibly change dependencies to handle on create new ticket/project submit function in hooks
 
-
-
-    const appendToEmailArray = async (teamId) => {
-      const { data, error } = await apiClient.fetchMemberList(teamId);
-      data.teamsData.map((member) =>
-        {
-          if(!availableMembers.includes(member.email))
-          {
-            setAvailableMembers((prev) => [...prev, member.email])
-          }
-        }
-      );
-    };
-
+  useEffect(() => {
+    fetchMemsForProject();
+  }, [selectedProject]);
 
   const fetchMemsForProject = async () => {
     if (selectedProject > -1) {
       setAvailableMembers([]);
-      let teams = currentProject.teams
+      let teams = currentProject.teams;
       teams?.map((t) => {
         appendToEmailArray(t);
       });
     }
   };
 
-
-
-
-
-  //Conditionally set the sorted projects to the list of projects only if projects is populated and the length of sorted projects is zero
-  if(projects.length > 0 && sortedProjects.length == 0)
-  {
-      setSortedProjects(projects)
-  }
-
   return (
     <div className="projects-page">
       {/* conditionally render the Modal to create a new team  */}
       {projectModal && <ProjectModal />}
-      {ticketModal && <TicketModal
+      {ticketModal && (
+        <TicketModal
           availableMembers={availableMembers}
           setCurrentProject={setSelectedProject}
           currentProject={selectedProject}
           handleOnProjectChange={handleOnProjectChange}
-        />}
+        />
+      )}
       {/* conditionally blur background depending on if modal is open */}
-      <div className={projectModal || ticketModal ? "background-blur" : "background"}>
+      <div
+        className={
+          projectModal || ticketModal ? "background-blur" : "background"
+        }
+      >
         <ProjectsOverview
-          projects={projects}
-          sortedProjects={sortedProjects}
-          setSortedProjects={setSortedProjects}
           handleOnProjectClick={handleOnProjectClick}
-          handleOnFilterChange={handleOnFilterChange}
           isLoading={isLoading}
           setProjectModal={setProjectModal}
         />
--- bugtracker-ui/src/contexts/project.jsx
+++ bugtracker-ui/src/contexts/project.jsx
@@ -7,10 +7,12 @@ const ProjectContext = createContext(null);
 export const ProjectContextProvider = ({ children }) => {
   const [projects, setProjects] = useState([]);
   const [projectModal, setProjectModal] = useState(false);
+  const [sortedProjects, setSortedProjects] = useState([]);
   const [currentProject, setCurrentProject] = useState({});
-  const [projectToEdit, setProjectToEdit] = useState({})
-  const [editing, setEditing] = useState(false)
+  const [projectToEdit, setProjectToEdit] = useState({});
+  const [editing, setEditing] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [sortValue, setSortValue] = useState("Most Tickets");
   const [error, setError] = useState("");
 
   const fetchProjects = async () => {
@@ -19,9 +21,13 @@ export const ProjectContextProvider = ({ children }) => {
     const { data, error } = await apiClient.listAllProjects();
     if (data) {
       setProjects(data.projectList);
-      if (data.projectList.length > 0) {
+      if (
+        data.projectList.length > 0 &&
+        Object.keys(currentProject).length == 0
+      ) {
         setCurrentProject(data.projectList[0]);
       }
+      sortProjects(data.projectList);
     }
     if (error) {
       setError(error);
@@ -32,10 +38,42 @@ export const ProjectContextProvider = ({ children }) => {
   // useEffect to fetch projects on initial load
   useEffect(() => {
     fetchProjects();
+    setSortedProjects(projects);
   }, []);
+
+  // useEffect to sort projects whenever sortValue changes
+  useEffect(() => {
+    sortProjects(projects);
+  }, [sortValue]);
+
+  // function to sort projects based on sort value
+  const sortProjects = (p) => {
+    if (sortValue === "Least Tickets") {
+      const results = p.sort((project1, project2) =>
+        project1.tickets.length > project2.tickets.length ? 1 : -1
+      );
+      setSortedProjects(results);
+    } else if (sortValue === "Most Tickets") {
+      const results = p.sort((project1, project2) =>
+        project1.tickets.length < project2.tickets.length ? 1 : -1
+      );
+      setSortedProjects(results);
+    } else if (sortValue === "Most Teams") {
+      const results = p.sort((project1, project2) =>
+        project1.teams.length > project2.teams.length ? 1 : -1
+      );
+      setSortedProjects(results);
+    } else if (sortValue === "Least Teams") {
+      const results = p.sort((project1, project2) =>
+        project1.teams.length < project2.teams.length ? 1 : -1
+      );
+      setSortedProjects(results);
+    }
+  };
 
   const clearProjectContext = () => {
     setProjects([]);
+    setSortedProjects([]);
     setProjectModal(false);
     setCurrentProject({});
     setIsLoading(false);
@@ -45,6 +83,8 @@ export const ProjectContextProvider = ({ children }) => {
   const projectValue = {
     projects,
     setProjects,
+    sortedProjects,
+    setSortedProjects,
     currentProject,
     setCurrentProject,
     fetchProjects,
@@ -53,9 +93,12 @@ export const ProjectContextProvider = ({ children }) => {
     isLoading,
     clearProjectContext,
     projectToEdit,
-   setProjectToEdit,
+    setProjectToEdit,
     editing,
     setEditing,
+    sortValue,
+    setSortValue,
+    sortProjects,
   };
 
   return (
